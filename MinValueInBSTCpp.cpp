ee533d7a2bbb39283319e62e2302dbefc6edf913
ajinkyajain23 <95224371+ajinkyajain23@users.noreply.github.com>
2024-03-30 07:36:36 +0530

    Create MinValueInBSTCpp.cpp

---
+++ MinValueInBSTCpp.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:   
+    int minValue(Node* root) {
+        // Code here
+        if(root->left == NULL){
+            return root->data;
+        }
+        
+        return minValue(root->left);
+    }
+};
