8944f2a036d94d7ef170ce10935c6eb8e022ae3b
ajinkyajain23 <95224371+ajinkyajain23@users.noreply.github.com>
2024-12-06 10:35:53 +0530

    Create HIndex.java

---
+++ HIndex.java
@@ -0,0 +1,12 @@
+public int hIndex(int[] citations) {
+        // code here
+        Arrays.sort(citations);
+        int n = citations.length;
+        for(int i = 0; i < n; ++i){
+            if(citations[i] >= n - i){
+                return n - i;
+            }
+        }
+        return 0;
+        
+    }
