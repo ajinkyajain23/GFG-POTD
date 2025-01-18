2e3773f7544c1216c426be60eaf1bd1041ba3b8a
ajinkyajain23 <95224371+ajinkyajain23@users.noreply.github.com>
2025-01-18 10:22:48 +0530

    Create ReverseList.java

---
+++ ReverseList.java
@@ -0,0 +1,12 @@
+Node reverseList(Node head) {
+        // code here
+        Node curr = head;
+        Node prev = null;
+        while(curr != null){
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
