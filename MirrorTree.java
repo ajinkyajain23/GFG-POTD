f0f99311b5a1ddb701b72498561005c418cca2cb
ajinkyajain23 <95224371+ajinkyajain23@users.noreply.github.com>
2025-02-05 04:51:59 +0530

    Create MirrorTree.java

---
+++ MirrorTree.java
@@ -0,0 +1,12 @@
+void mirror(Node node) {
+        // Your code here
+        if(node == null){
+            return;
+        }
+        
+        Node temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+        mirror(node.left);
+        mirror(node.right);
+}
